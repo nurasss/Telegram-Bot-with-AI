96e6022bb485db154fe68dd78623f6f687cfcdd3
nurasss <127543275+nurasss@users.noreply.github.com>
2025-11-26 21:57:36 +0500

    Replace API tokens with placeholders

--- Al-Business Analyst/ai_analyst/config.py
+++ Al-Business Analyst/ai_analyst/config.py
@@ -1,6 +1,6 @@
 # Токен от BotFather
-TELEGRAM_TOKEN = "8504096734:AAEBAc7fk_F4PtbHOGTcfDRyDFvyerst58U"
+TELEGRAM_TOKEN = "ВАШ_ТОКЕН"
 
 # Ключ из Google AI Studio
-GOOGLE_API_KEY = "AIzaSyDhQW_egvoEUXGWgyIOfl8IDC2FQIRVgK4"
+GOOGLE_API_KEY = "ВАШ_ТОКЕН"
 
